1bc72e22969fbd6ae3eb7998f0aa9e24b756e940
Eugenio Cano <eucanomunoz@outlook.com>
2024-08-19 17:05:53 +0200

    added some test cases

--- tests/test_request.rs
+++ tests/test_request.rs
@@ -32,3 +32,45 @@ This is the body of the request.";
 
     assert_eq!(request::Request::try_from(req_str).unwrap(), req);
 }
+
+#[test]
+fn empty_request() {
+    let req_str = "";
+    let req = request::Request::try_from(req_str);
+
+    match req.unwrap_err() {
+        servidor_http::Error::RequestError(servidor_http::request::RequestError::InvalidRequest(_)) => assert!(true),
+        _ => assert!(false)
+    }
+    
+}
+
+macro_rules! generate_request_method_type_tests {
+    (
+        $($test_name:ident, $method:ident);*
+    ) => {
+       $(
+            #[test]
+            fn $test_name () {
+                let req_str = format!("{} /index.html HTTP/1.1\n", stringify!($method));
+                let req = request::Request::try_from(req_str.as_str()).unwrap();
+
+                assert_eq!(req, request::Request::new(request::RequestMethod::$method, "/index.html"));
+            }
+       )*
+    };
+}
+
+generate_request_method_type_tests!(get_request, GET; post_request, POST; put_request, PUT; delete_request, DELETE; head_request, HEAD);
+
+#[test]
+fn request_with_invalid_method() {
+    let method = "Invalid,";
+    let req_str = format!("{} /index.html HTTP/1.1\n", method);
+    let req = request::Request::try_from(req_str.as_str());
+
+    match req.unwrap_err() {
+        servidor_http::Error::RequestError(servidor_http::request::RequestError::InvalidRequestMethod(mtd)) => assert_eq!(mtd, method),
+        _ => assert!(false)
+    }
+}
